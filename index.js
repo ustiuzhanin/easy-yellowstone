19b1cc80dea42e661ef004ca2249b3465bf30100
Slava <ucheba00@gmail.com>
2019-06-29 02:10:10 -0600

    heroku .8

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ if (process.env.NODE_ENV === 'production') {
   console.log('server running');
   app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   })
 }
